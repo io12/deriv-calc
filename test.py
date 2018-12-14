a5e68a1b1d42d32c78ba12ed0551be139a859c77
Benjamin Levy <blevy@protonmail.com>
2018-12-14 18:33:33 -0500

    Add derivative unit tests

---
+++ test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import unittest
+
+import expr
+
+
+class Test(unittest.TestCase):
+    def test_deriv(self):
+        def deriv_eq(u, du):
+            self.assertEqual(str(expr.parse(u).deriv().simpl()), du)
+        deriv_eq('c', '0')
+        deriv_eq('x', '1')
+        deriv_eq('a * x', 'a')
+        deriv_eq('x ** 2', 'x * 2')  # TODO
+        deriv_eq('sqrt(x)', '1/2 * 1 / x^1/2')  # TODO
+        deriv_eq('e^x', 'e^(x - 1) * e * ln(e)')  # TODO
+        deriv_eq('a^x', 'a^(x - 1) * a * ln(a)')  # TODO
+        deriv_eq('ln(x)', '1 / x')
+        deriv_eq('sin(x)', 'cos(x)')
+        deriv_eq('cos(x)', '-sin(x)')
+        deriv_eq('tan(x)', 'sec(x)^2')
+        deriv_eq('asin(x)', '1 / (1 - x^2)^1/2')  # TODO
+        deriv_eq('x^n', 'x^(n - 1) * n')  # TODO
+        deriv_eq('1 / x', '-1 / x^2')
+        deriv_eq('5 * x^2 + x^3 - 7 * x^4',
+                 '10 * x + 3 * x^2 - 28 * x^3')  # TODO
+
+
+if __name__ == '__main__':
+    unittest.main()
